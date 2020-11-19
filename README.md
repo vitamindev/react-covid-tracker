9f73d85c640ca76f0ded31df0b33ac81e4f65f82
Benoit Paul <benoitpaul@Benoits-MacBook-Pro.local>
2020-11-18 22:58:41 -0500

    set default url

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # react-covid-tracker
 
 A simple COVID tracker, built with material-ui, react-chartjs-2, react-google-charts and react-router
+
 [Demo](https://amazing-kilby-3ad470.netlify.app/)
 
 [World timeline demo](https://amazing-kilby-3ad470.netlify.app/world/timeline)
--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,9 +1,8 @@
 import React from "react";
 
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Country from "../Country/Country";
 import World from "../World/World";
-import Home from "../Home/Home";
 
 const MainContent = ({ countryList, timeline, handleDrawerToggle }) => {
   return (
@@ -22,7 +21,7 @@ const MainContent = ({ countryList, timeline, handleDrawerToggle }) => {
         />
       </Route>
       <Route exact path="/">
-        <Home />
+        <Redirect to="/world/timeline" /> :
       </Route>
     </Switch>
   );
